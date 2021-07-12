86bf4021607e1741d6997b3cac63b89d447df51b
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2021-07-12 22:59:34 +0300

    gulp-sass v5 updated

--- Pulse_step_9/gulpfile.js
+++ Pulse_step_9/gulpfile.js
@@ -1,6 +1,6 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
