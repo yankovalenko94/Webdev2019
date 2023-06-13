8707b259619772f5678134f6574f610b2a6a8dfa
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2023-06-13 13:06:54 +0100

    packages update

--- Pulse_step_11/dist/js/script.js
+++ Pulse_step_11/dist/js/script.js
@@ -81,7 +81,7 @@ $(document).ready(function(){
     validateForms('#consultation form');
     validateForms('#order form');
 
-    $('input[name=phone]').mask("+7 (999) 999-99-99");
+    $('input[name=phone]').mask("+730 (999) 999-99-99");
 
     $('form').submit(function(e) {
         e.preventDefault();
